bb6a08b1ab9946792a1a7290748ffcd7816b43e9
donghwan <kdhe9797@naver.com>
2025-03-23 01:04:10 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
+    credentials: true,
+  })
   await app.listen(process.env.PORT ?? 4000)
 }
 bootstrap()
